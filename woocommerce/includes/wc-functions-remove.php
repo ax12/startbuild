041d66ea65428fa42b6a37d85063e06716278135
Max <2903186@gmail.com>
2019-08-08 21:12:16 +0500

    style fo wc

--- woocommerce/includes/wc-functions-remove.php
+++ woocommerce/includes/wc-functions-remove.php
@@ -1,7 +1,15 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: max
- * Date: 08.08.2019
- * Time: 17:14
+ * Set WooCommerce image dimensions upon theme activation
  */
+// Remove each style one by one
+add_filter( 'woocommerce_enqueue_styles', 'jk_dequeue_styles' );
+function jk_dequeue_styles( $enqueue_styles ) {
+    unset( $enqueue_styles['woocommerce-general'] );	// Remove the gloss
+    unset( $enqueue_styles['woocommerce-layout'] );		// Remove the layout
+    unset( $enqueue_styles['woocommerce-smallscreen'] );	// Remove the smallscreen optimisation
+    return $enqueue_styles;
+}
+
+// Or just remove them all in one line
+add_filter( 'woocommerce_enqueue_styles', '__return_false' );
