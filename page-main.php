9022c663cadb15f80f9e2ede8aa25dd823fc23e6
Max <2903186@gmail.com>
2019-08-08 14:57:33 +0500

    h1 = master

--- page-main.php
+++ page-main.php
@@ -2,23 +2,24 @@
 <?php
 get_header('main');
 ?>
-<h1>Шаблон главной страницы (branch woo)</h1>
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+    <h1>Шаблон главной страницы (branch woo)</h1>
 
-			get_template_part( 'template-parts/content', 'page' );
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main">
+
+            <?php
+            while (have_posts()) :
+                the_post();
+
+                get_template_part('template-parts/content', 'page');
 
 
+            endwhile; // End of the loop.
+            ?>
 
-		endwhile; // End of the loop.
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+        </main><!-- #main -->
+    </div><!-- #primary -->
 
 <?php
 get_footer();
