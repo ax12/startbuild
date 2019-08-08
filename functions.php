ff9f9c678e9b9b4323e0798fc00dd1aae88c2844
Max <2903186@gmail.com>
2019-08-08 18:20:30 +0500

    add carbon fields

--- functions.php
+++ functions.php
@@ -1,4 +1,24 @@
 <?php
+/**START adding carbon-fields**/
+add_action('after_setup_theme', 'crb_load');
+function crb_load()
+{
+    require_once('/includes/carbon-fields/vendor/autoload.php');
+    \Carbon_Fields\Carbon_Fields::boot();
+}
+/**END adding carbon-fields**/
+
+/**START add my files for carbon-fields*/
+add_action('carbon_fields_register_fields', 'crb_register_custom_fields'); // Для версии 2.0 и выше
+//add_action( 'carbon_register_fields', 'crb_register_custom_fields' ); // Для версии 1.6 и ниже
+function crb_register_custom_fields()
+{
+    // путь к пользовательскому файлу определения поля (полей), измените под себя
+    require get_template_directory() . '/includes/custom-field-options/matabox.php';
+    require  get_template_directory() .'/includes/custom-field-options/them-options.php';
+}
+
+/**END adding developers files for carbon-fields*/
 /**
  connect theme settings fom file theme-settings.php
  */
---
+++ includes/custom-field-options/matabox.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: max
+ * Date: 08.08.2019
+ * Time: 17:42
+ */
