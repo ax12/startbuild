bfc586c75ee73594b870d75c93df11788579d8fd
max <2903186@gmail.com>
2019-10-10 16:49:31 +0500

    Row About us is complete

--- includes/custom-field-options/matabox.php
+++ includes/custom-field-options/matabox.php
@@ -1,11 +1,18 @@
 <?php
+
 use Carbon_Fields\Container;
 use Carbon_Fields\Field;
 
 Container::make('post_meta', 'Настройки главной страницы сайта "Апельсиновый Фазан"')
     ->show_on_template('page-main.php')
     ->add_fields(array(
-        Field::make('map', 'crb_location')->set_position(37.423156, -122.084917, 14),
-
-        Field::make('image', 'crb_photo'),
+        Field::make('text', 'crb_header_about', __('Заголовок первого раздела (о нас)'))->set_default_value('О нас')->set_required(true)
+            ->set_width(30),
+        Field::make('text', 'crb_button_name', __('Надпись на кнопке'))->set_default_value('Читать далее')->set_required(true)
+            ->set_width(30),
+        Field::make('text', 'crb_button_link', __('Ссылка на кнопке')) ->set_default_value('/about')->set_required(true)
+            ->set_width(30),
+        Field::make( 'rich_text', 'crb_about_description', __( 'Текст в первом разделе (о нас)' ) )
+            ->set_width(100),
     ));
+
--- page-main.php
+++ page-main.php
@@ -5,12 +5,25 @@ get_header('main');
 
     <div class="container-fluid">
         <div id="content">
-            <div class="row row-about-us"></div>
+            <div class="row row-about-us pt-md-5">
+                <div class="col-md-1"></div>
+            <div class="col-md-6 text-center">
+                <h2 class="pt-md-5">- <?php echo carbon_get_the_post_meta ('crb_header_about') ?> -</h2>
+                <?php echo carbon_get_the_post_meta ('crb_about_description') ?>
+                <a href="<?php echo carbon_get_the_post_meta ('crb_button_link') ?>"><button type="button" class="btn btn-success mx-btn-color"><?php echo carbon_get_the_post_meta ('crb_button_name') ?></button></a>
+            </div>
+
+            <div class="col-md-5">
+                    <div class="mx-square-line mt-md-5 justify-content-center">
+                       <span class="text-center"><?php echo carbon_get_the_post_meta ('crb_header_about') ?></span>
+
+                    </div>
 
 
-        </div>
+            </div>
+
+        </div> <!--end row-->
     </div>
-
 
 
 <?php
